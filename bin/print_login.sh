1f6c323a70d38db1738db6e4ec5d5547aaaf4dd6
Quincy Cheng <quincy.cheng@gmail.com>
2018-03-21 00:39:02 +0800

    fixed spring boot

--- bin/print_login.sh
+++ bin/print_login.sh
@@ -21,15 +21,15 @@ URL:  http://${JENKINS_URL}
 User: ${JENKINS_USER}
 Pass: ${JENKINS_PASS}
 
-[Artifactory]
-URL:  http://${ARTIFACTORY_URL}
-User: ${ARTIFACTORY_USER}
-Pass: ${ARTIFACTORY_PASS}
-
 [SonarQube]
 URL:  http://${SONAR_URL}
 User: ${SONAR_USER}
 Pass: ${SONAR_PASS}
+
+[Artifactory]
+URL:  http://${ARTIFACTORY_URL}
+User: ${ARTIFACTORY_USER}
+Pass: ${ARTIFACTORY_PASS}
 
 [Scope]
 URL:  http://${SCOPE_URL}
